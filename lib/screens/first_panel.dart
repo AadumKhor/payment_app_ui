91eedf8c9762936cac574167803882e16664177f
Aayush Malhotra <37381075+AadumKhor@users.noreply.github.com>
2019-03-24 04:26:06 +0530

    FirstPanel complete

--- lib/screens/first_panel.dart
+++ lib/screens/first_panel.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
 // *******************************************
 /* */
@@ -13,84 +16,229 @@ class _FirstPanelState extends State<FirstPanel> {
   @override
   Widget build(BuildContext context) {
     return new Container(
-            decoration: new BoxDecoration(
-                gradient: LinearGradient(
-      begin: Alignment.centerLeft,
-      end: Alignment.centerRight,
-      colors: [Colors.green[100], Colors.teal[100]])),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                //this Container contains the sign in info of the parent
-                //it will show the image fetched from sign in if any
-                //and will also show the name and id of the parent
-                new Container(
-    height: 200.0,
-    width: double.infinity,
-    decoration: new BoxDecoration(
-        gradient: LinearGradient(
-            begin: Alignment.centerLeft,
-            end: Alignment.centerRight,
-            colors: [Colors.green[100], Colors.blue[100]])),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        new Container(
-          width: 20.0,
-          height: 20.0,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10.0),
-              color: Colors.white),
-        )
-      ],
-    ),
+        width: double.infinity,
+        decoration: new BoxDecoration(color: Color(0xFF4E455E)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 30.0, horizontal: 10.0),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    width: 75.0,
+                    height: 75.0,
+                    decoration: BoxDecoration(
+                        border: Border.all(width: 2.0, color: Colors.white),
+                        borderRadius: BorderRadius.circular(20.0)
+                        // add image if you want to
+                        ),
+                  ),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  new Text(
+                    'Hi , Veronica',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 2.0),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: new Text(
+                  'Your total balance is : ',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      letterSpacing: 2.0,
+                      fontWeight: FontWeight.w200),
                 ),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 4.0, left: 8.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: new Text(
+                  '\$162',
+                  style: TextStyle(
+                      fontSize: 40.0,
+                      letterSpacing: 2.0,
+                      fontWeight: FontWeight.w900),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // crypto widget
+                  Container(
+                    width: 150.0,
+                    height: 200.0,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: Color(0XFF4821F9)),
+                    child: Stack(
+                      children: <Widget>[
+                        // the design on the back of each card
 
-                //the various options begin from here
-                //we can modify these options later on
+                        Positioned(
+                            top: 0.0,
+                            child: ClipPath(
+                              clipper: WaveClipperTwo(),
+                              child: Container(
+                                height: 10.0,
+                                color: Colors.white30,
+                              ),
+                            )),
+                        BackdropFilter(
+                          filter: ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
+                          child: Container(
+                            width: 150.0,
+                            height: 200.0,
+                            decoration: BoxDecoration(
+                                color: Colors.white.withOpacity(0.15),
+                                borderRadius: BorderRadius.circular(20.0)),
+                          ),
+                        ),
+                        // original text
+                        Center(
+                          child: new Text(
+                            '\$22.00',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white,
+                                fontSize: 30.0),
+                          ),
+                        ),
+                        // the text behind the original text
+                        Positioned(
+                          top: 30.0,
+                          left: -50,
+                          child: new Text(
+                            '\$22.00',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                color: Colors.white10,
+                                fontSize: 90.0),
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 20.0,
+                          left: 40.0,
+                          child: new Text(
+                            'Crypto',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w300,
+                                color: Colors.white,
+                                fontSize: 20.0),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    width: 40.0,
+                  ),
+                  // currency widget
+                  Container(
+                    width: 150.0,
+                    height: 200.0,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: Color(0XFFFA3098)),
+                                            child: Stack(
+                      children: <Widget>[
+                        // the design on the back of each card
 
-                Padding(
-    padding: const EdgeInsets.all(16.0),
-    child: new FlatButton(
-      child: new Text(
-        'Test Score',
-        style: TextStyle(fontSize: 20.0),
-      ),
-      onPressed: () {},
-    ),
-                ),
-                Padding(
-    padding: const EdgeInsets.all(16.0),
-    child: new FlatButton(
-      child: new Text(
-        'Test Score',
-        style: TextStyle(fontSize: 20.0),
-      ),
-      onPressed: () {},
-    ),
-                ),
-                Padding(
-    padding: const EdgeInsets.all(16.0),
-    child: new FlatButton(
-      child: new Text(
-        'Test Score',
-        style: TextStyle(fontSize: 20.0),
-      ),
-      onPressed: () {},
-    ),
-                ),
-                Padding(
-    padding: const EdgeInsets.all(16.0),
-    child: new FlatButton(
-      child: new Text(
-        'Test Score',
-        style: TextStyle(fontSize: 20.0),
-      ),
-      onPressed: () {},
-    ),
-                )
-              ],
-            ));
-    }
+                        Positioned(
+                            top: 0.0,
+                            child: ClipPath(
+                              clipper: WaveClipperTwo(),
+                              child: Container(
+                                height: 10.0,
+                                color: Colors.white30,
+                              ),
+                            )),
+                        BackdropFilter(
+                          filter: ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
+                          child: Container(
+                            width: 150.0,
+                            height: 200.0,
+                            decoration: BoxDecoration(
+                                color: Colors.white.withOpacity(0.15),
+                                borderRadius: BorderRadius.circular(20.0)),
+                          ),
+                        ),
+                        // original text
+                        Center(
+                          child: new Text(
+                            '\$135.00',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white,
+                                fontSize: 30.0),
+                          ),
+                        ),
+                        // the text behind the original text
+                        Positioned(
+                          top: 30.0,
+                          left: -50,
+                          child: new Text(
+                            '\$135.00',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                color: Colors.white10,
+                                fontSize: 90.0),
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 20.0,
+                          left: 40.0,
+                          child: new Text(
+                            'Dollars',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w300,
+                                color: Colors.white,
+                                fontSize: 20.0),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
+  }
 }
+
+// class CircleClipper extends CustomClipper<ClipOval>{
+//   @override
+//   ClipOval getClip(Size size) {
+
+//     return null;
+//   }
+
+//   @override
+//   bool shouldReclip(CustomClipper<ClipOval> oldClipper) {
+//     return true;
+//   }
+// }
--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -8,7 +8,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen>
     with SingleTickerProviderStateMixin {
-  static const header_height = 200.0;
+  static const header_height = 250.0;
   AnimationController controller;
 
   Animation<RelativeRect> getAniamtionPanel(BoxConstraints constraints) {
@@ -42,148 +42,66 @@ class _HomeScreenState extends State<HomeScreen>
 
   Widget bothPanels(BuildContext context, BoxConstraints constraints) {
     // controller.forward();
-    return new Container(
-      child: new Stack(
-        children: <Widget>[
-          //***********************************
-          //This is the container that appears after we click on the drawer icon.
-          //It contains the various options our user will have to navigate our app.
-          //Color is set ot theme color for uniformity
-          //*********************************
-          FirstPanel(),
-          // new Container(
-          //     decoration: new BoxDecoration(
-          //         gradient: LinearGradient(
-          //             begin: Alignment.centerLeft,
-          //             end: Alignment.centerRight,
-          //             colors: [Colors.green[100], Colors.teal[100]])),
-          //     child: Column(
-          //       mainAxisAlignment: MainAxisAlignment.start,
-          //       children: <Widget>[
-          //         //this Container contains the sign in info of the parent
-          //         //it will show the image fetched from sign in if any
-          //         //and will also show the name and id of the parent
-          //         new Container(
-          //           height: 200.0,
-          //           width: double.infinity,
-          //           decoration: new BoxDecoration(
-          //               gradient: LinearGradient(
-          //                   begin: Alignment.centerLeft,
-          //                   end: Alignment.centerRight,
-          //                   colors: [Colors.green[100], Colors.blue[100]])),
-          //           child: Column(
-          //             mainAxisAlignment: MainAxisAlignment.center,
-          //             crossAxisAlignment: CrossAxisAlignment.center,
-          //             children: <Widget>[
-          //               new Container(
-          //                 width: 20.0,
-          //                 height: 20.0,
-          //                 decoration: BoxDecoration(
-          //                     borderRadius: BorderRadius.circular(10.0),
-          //                     color: Colors.white),
-          //               )
-          //             ],
-          //           ),
-          //         ),
+    return Scaffold(
+      body: new Container(
+        child: new Stack(
+          children: <Widget>[
+            //***********************************
+            //This is the container that appears after we click on the drawer icon.
+            //It contains the various options our user will have to navigate our app.
+            //Color is set ot theme color for uniformity
+            //*********************************
+            FirstPanel(),
 
-          //         //the various options begin from here
-          //         //we can modify these options later on
-          //         //TODO: Make them better if possible
-          //         Padding(
-          //           padding: const EdgeInsets.all(16.0),
-          //           child: new FlatButton(
-          //             child: new Text(
-          //               'Test Score',
-          //               style: TextStyle(fontSize: 20.0),
-          //             ),
-          //             onPressed: () {},
-          //           ),
-          //         ),
-          //         Padding(
-          //           padding: const EdgeInsets.all(16.0),
-          //           child: new FlatButton(
-          //             child: new Text(
-          //               'Test Score',
-          //               style: TextStyle(fontSize: 20.0),
-          //             ),
-          //             onPressed: () {},
-          //           ),
-          //         ),
-          //         Padding(
-          //           padding: const EdgeInsets.all(16.0),
-          //           child: new FlatButton(
-          //             child: new Text(
-          //               'Test Score',
-          //               style: TextStyle(fontSize: 20.0),
-          //             ),
-          //             onPressed: () {},
-          //           ),
-          //         ),
-          //         Padding(
-          //           padding: const EdgeInsets.all(16.0),
-          //           child: new FlatButton(
-          //             child: new Text(
-          //               'Test Score',
-          //               style: TextStyle(fontSize: 20.0),
-          //             ),
-          //             onPressed: () {},
-          //           ),
-          //         )
-          //       ],
-          //     )),
-          // ******************************************
-          // the second panel that contains the list of items
-          // ******************************************
-          PositionedTransition(
-            rect: getAniamtionPanel(constraints),
-            child: new Material(
-              elevation: 12.0,
-              borderRadius: new BorderRadius.only(
-                  topLeft: Radius.circular(30.0),
-                  topRight: Radius.circular(30.0)),
-              child: new Column(
-                children: <Widget>[
-                  new Container(
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(30.0),
-                            topRight: Radius.circular(30.0))),
-                    height: 30.0,
-                    child: new Center(
-                            child: IconButton(
-                              onPressed: (){
-                                controller.fling(velocity: isPanelVisible?-1.0:1.0);
-                              },
-                              icon : Icon(Icons.linear_scale),
-                              color: Color(0xFFAEA6C4),
-                            
-                            )),
-                  ),
-                  Expanded(
-                    child: new Container(
-                        height: header_height,
-                        decoration: new BoxDecoration(
-                            gradient: LinearGradient(
-                                begin: Alignment.centerLeft,
-                                end: Alignment.centerRight,
-                                colors: [Colors.pink[200], Colors.blue[200]])),
-                        child: ListView.builder(
-                          itemBuilder: (BuildContext context, int index) {
-                            return Container(
-                              width: double.infinity,
-                              height: 160.0,
-                              child: Icon(Icons.ac_unit),
-                              decoration: BoxDecoration(color: Colors.white),
-                            );
-                          },
-                        )),
-                  )
-                ],
+            // ******************************************
+            // the second panel that contains the list of items
+            // ******************************************
+
+            PositionedTransition(
+              rect: getAniamtionPanel(constraints),
+              child: new Material(
+                elevation: 12.0,
+                borderRadius: new BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0)),
+                child: new Column(
+                  children: <Widget>[
+                    new Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(30.0),
+                              topRight: Radius.circular(30.0))),
+                      height: 30.0,
+                      child: new Center(
+                          child: IconButton(
+                        onPressed: () {
+                          controller.fling(
+                              velocity: isPanelVisible ? -1.0 : 1.0);
+                        },
+                        icon: Icon(Icons.maximize),
+                        iconSize: 40.0,
+                        color: Color(0xFFAEA6C4),
+                      )),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemBuilder: (BuildContext context, int index) {
+                          return Container(
+                            width: double.infinity,
+                            height: 160.0,
+                            child: Icon(Icons.ac_unit),
+                            decoration: BoxDecoration(color: Colors.white),
+                          );
+                        },
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
